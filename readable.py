766914f77be6ca0867b0bf3b8780cfba67f6a6c9
aztahmid <aztahmid@gmail.com>
2021-01-02 16:54:54 -0500

    This is a preliminary version of the program which can use a text file

--- readable.py
+++ readable.py
@@ -1,7 +1,70 @@
+# Check to see if it's basic English
+def inputyn(item):
+    acceptableResponse = False
+    response = ""
+    while(not acceptableResponse):
+        response = input(f"{item}: ")
+        if response.lower() == 'y':
+            acceptableResponse = True
+            return True
+        elif response.lower() == 'n':
+            acceptableResponse = True
+            return False
+        else:
+            print(f"That is not an acceptable response, do you know this word? (Y/n):")
 
+    return False
+
+def textCleaner(paragraph):
+    NON_LETTER_CHARACTERS = "!@#$%^&*()[]{}_+<>?:.,;/0123456789"
+    cleanParagraph_text = ""
+    cleanParagraph1 = []
+    cleanParagraph2 = []
+    finalParagraph = []
+
+    for a in paragraph:
+        if a not in NON_LETTER_CHARACTERS:
+            cleanParagraph_text += a
+        else:
+            cleanParagraph_text += " "
+
+    cleanParagraph1 = cleanParagraph_text.split()
+
+    for i in range(len(cleanParagraph1)):
+        cleanParagraph1[i] = cleanParagraph1[i].lower()
+
+    for i in cleanParagraph1:
+        if i not in cleanParagraph2:
+            cleanParagraph2.append(i)
+
+    cleanParagraph2.sort()
+    finalParagraph = cleanParagraph2
+
+    return finalParagraph
+
+
+def basicEnglishChecker(checker):
+    isBasic = False
+
+    try:
+        basicWords.index(checker.lower())
+        isBasic = True
+    except ValueError:
+        isBasic = False
+
+    isEnglish = False
+
+    if isBasic:
+        isEnglish = True
+    else:
+        try:
+            allEnglish.index(checker.lower())
+            isEnglish = True
+        except ValueError:
+            isEnglish = False
+    return isEnglish, isBasic
 
 # The purpose of this section is to import a list of basic English words, clean it, and sort it.
-
 basicWords_file = open("/home/atahmid/PycharmProjects/readable/basic_wordlist", "r")
 basicWords = basicWords_file.readlines()
 
@@ -20,32 +83,44 @@ for i in range(len(allEnglish)):
     allEnglish[i] = allEnglish[i].rstrip("\n")
 allEnglish.sort()
 
+# Now we import the article
+article_file = open("/home/atahmid/PycharmProjects/readable/article", "r")
+article_whole = article_file.read()
 
-# Take input
-checker = input()
+article = textCleaner(article_whole)
 
-# Check to see if it's basic English
-isBasic = False
+isEnglishWord = False
+isBasicWord = False
 
-try:
-    basicWords.index(checker.lower())
-    isBasic = True
-except ValueError:
-    isBasic = False
+easyWords = []
+complexWordsCandidates = []
+complexWords = []
 
-isEnglish = False
+for a in article:
+    isEnglishWord, isBasicWord = basicEnglishChecker(a)
+    if isBasicWord:
+        easyWords.append(a)
+    elif isEnglishWord:
+        complexWordsCandidates.append(a)
+    else:
+        pass
 
-if isBasic:
-    isEnglish = True
-else:
-    try:
-        allEnglish.index(checker.lower())
-        isEnglish = True
-    except ValueError:
-        isEnglish = False
+complexWordsCandidates.sort()
+
+print("For each of the following words, indicate, do you know what this word means? (Y/n)")
+for item in complexWordsCandidates:
+    wordKnown = inputyn(item)
+    if wordKnown:
+        easyWords.append(item)
+    else:
+        complexWords.append(item)
 
 
-print(f"English: {isEnglish}, Basic:{isBasic}")
+
+easeRatio = float(len(easyWords))/(float(len(complexWords)) + float(len(easyWords)))
+easeRatio = round(easeRatio, 2)
+
+print(f"The ease ratio is {easeRatio}")
 
 
 
